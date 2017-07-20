8f69db920817ed6d9e1f30fb85c9c48a9d81bcd1
Byzantine <morbyzantine@gmail.com>
2017-07-19 23:35:55 -0700

    Half-finished 2-1, no idea how to work on variadic Args

--- traits-and-types/exercise.h
+++ traits-and-types/exercise.h
@@ -1,6 +1,92 @@
 #include <iterator>
 
+// 2-0
+// add_const_ref
+template <bool IsRef, class T>
+struct add_const_ref_impl {
+}; 
+
 template <class T>
 struct add_const_ref {
-  using type = T;
+  using type = typename add_const_ref_impl<false, T>::type;
 };
+
+// This is using partial tempalte specialization.
+template <class T>
+struct add_const_ref <T&> {
+  using type = typename add_const_ref_impl<true, T>::type;
+};
+
+template <class T>
+struct add_const_ref_impl<true, T> {
+  using type = T;
+}; 
+
+template <class T>
+struct add_const_ref_impl<false, T> {
+  using type = const T&;
+}; 
+
+// 2-1
+// replace_type
+template <class C, class X, class Y, bool IsSame>
+struct replace_type_impl {
+  using type = C;
+};
+
+template <class C, class X, class Y>
+struct replace_type {
+  using type = typename replace_type_impl<C, X, Y, std::is_same<C, X>::value>::type;
+};
+
+// Need to introduce another intermediate func to avoid recursion.
+template <class C, class X, class Y>
+struct replace_type_im {
+  using type = C;
+};
+
+template <class C, class X, class Y>
+struct replace_type_impl<C, X, Y, true> {
+  using type = Y;
+};
+
+template <class C, class X, class Y>
+struct replace_type_impl<C, X, Y, false> {
+  using type = typename replace_type_im<C, X, Y>::type;
+};
+
+template <class C, class X, class Y>
+struct replace_type_im<C*, X, Y> {
+  using type = typename std::add_pointer<typename replace_type_impl<C, X, Y, std::is_same<C, X>::value>::type>::type;
+};
+
+template <class C, class X, class Y>
+struct replace_type_im<C&, X, Y> {
+  using type = typename std::add_lvalue_reference<typename replace_type_impl<C, X, Y, std::is_same<C, X>::value>::type>::type;
+};
+
+template <class C, class X, class Y>
+struct replace_type_im<C&&, X, Y> {
+  using type = typename std::add_rvalue_reference<typename replace_type_impl<C, X, Y, std::is_same<C, X>::value>::type>::type;
+};
+
+template <class C, class X, class Y, size_t N>
+struct replace_type_im<C[N], X, Y> {
+  using type = typename replace_type_impl<C, X, Y, std::is_same<C, X>::value>::type [N];
+};
+
+template <class R, class X, class Y, class T, class ... Args>
+struct replace_type_im<R(*)(T, Args...), X, Y> {
+  using typeT = typename replace_type_impl<R, X, Y, std::is_same<R, X>::value>::type (*)
+      (typename replace_type_impl<T, X, Y, std::is_same<T, X>::value>::type);
+  using typeArgs = typename replace_type_im<R(*)(Args...), X, Y>::type; 
+  //TODO(yejiayu): join this two type, need function traits?.
+  using type = short;
+};
+
+template <class R, class X, class Y, class T>
+struct replace_type_im<R(*)(T), X, Y> {
+  using type = typename replace_type_impl<R, X, Y, std::is_same<R, X>::value>::type (*)(
+      typename replace_type_impl<T, X, Y, std::is_same<T, X>::value>::type);
+};
+
--- traits-and-types/exercise_test.cc
+++ traits-and-types/exercise_test.cc
@@ -1,5 +1,56 @@
+#include "exercise.h"
+
+#include <iostream>
+
+#include <type_traits>
 #include "gtest/gtest.h"
 
-TEST(AddConstRefTest, Base) {
+template <typename T> std::string type_name();
 
+// 2-0
+TEST(AddConstRefTest, Base) {
+  bool result = std::is_same<add_const_ref<int>::type, const int&>::value;
+  EXPECT_TRUE(result);
+  result = std::is_same<add_const_ref<int &>::type, int>::value;
+  EXPECT_TRUE(result);
+  result = std::is_same<add_const_ref<const int>::type, const int&>::value;
+  EXPECT_TRUE(result);
+}
+
+// 2-1
+TEST(ReplaceType, Base) {
+  bool result = std::is_same<replace_type<void*, void, int>::type, int*>::value;
+  EXPECT_TRUE(result);
+
+  result = std::is_same<replace_type<char&, char, int>::type, int&>::value;
+  EXPECT_TRUE(result);
+
+  // This case introduce extra layer of abstraction. 
+  result = std::is_same<replace_type<char&, char&, int>::type, int>::value;
+  EXPECT_TRUE(result);
+
+  result = std::is_same<replace_type<char&&, char, int>::type, int&&>::value;
+  EXPECT_TRUE(result);
+
+  result = std::is_same<replace_type<int const*[10], int const, long>::type, long*[10]>::value;
+  EXPECT_TRUE(result);
+
+  result = std::is_same<replace_type<char& (*)(int&), char&, long&>::type, long&(*)(int&)>::value;
+  EXPECT_TRUE(result);
+
+  result = std::is_same<replace_type<char& (*)(char&), char&, long&>::type, long&(*)(long&)>::value;
+  EXPECT_TRUE(result);
+
+  // result = std::is_same<replace_type<char& (*)(char&, char&), char&, long&>::type, long&(*)(long&, char&)>::value;
+  // EXPECT_TRUE(result);
+}
+
+// 2-3
+TEST(TypeDescriptor, Base) {
+  // // prints "int"
+  // std::cout << type_descriptor<int>();
+  // // prints "char*"
+  // std::cout << type_descriptor<char*>();
+  // // prints "long const*&"
+  // std::cout << type_descriptor<long const*&>();
 }
